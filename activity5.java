6e4091a20d5bb3fac52b9c43c395f0a48186dd80
jyothikumarjk7 <82643356+jyothikumarjk7@users.noreply.github.com>
2021-08-13 17:24:37 +0530

    Add files via upload

---
+++ activity5.java
@@ -0,0 +1,18 @@
+package activity5;
+
+import java.io.*;
+import java.util.Scanner;
+public class activity5 {
+    public static void main(String[] args) {
+        int i,sum=0,N;
+        float average;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 10 numbers to find the average:");
+        for(i=0;i<10;i++){
+            N=sc.nextInt();
+            sum=sum+N;
+        }
+        average=sum/(float)10;
+        System.out.println("The average of 10 number "+"is "+average);
+    }
+}
